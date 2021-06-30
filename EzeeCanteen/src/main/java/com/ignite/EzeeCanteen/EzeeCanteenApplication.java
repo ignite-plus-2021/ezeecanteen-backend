e857982b7bd0e83bedda3ca0ef1a1e20afe8e477
Manassa2000 <73513084+Manassa2000@users.noreply.github.com>
2021-06-30 14:58:34 +0530

    Add files via upload

---
+++ EzeeCanteen/src/main/java/com/ignite/EzeeCanteen/EzeeCanteenApplication.java
@@ -0,0 +1,13 @@
+package com.ignite.EzeeCanteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzeeCanteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzeeCanteenApplication.class, args);
+	}
+
+}
